5dfde94cb84dbc970dc8fb8503f15144fa336aa9
unknown <congnguyen1496@gmail.com>
2025-06-28 00:13:03 +0700

    updated

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import fetch from "node-fetch";
-
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -32,14 +33,28 @@ app.post("/submit", async (req, res) => {
 // Route GET - lấy dữ liệu từ Google Sheet
 app.get("/data", async (req, res) => {
   try {
-    const response = await fetch("https://v1.nocodeapi.com/quin/google_sheets/nBVUPKUFKhYxExOB?tabId=Sheet1&perPage=100&page=1&fields=Name,Email");
+    const apiKey = process.env.NOCODE_API_KEY;
+    const url = `https://v1.nocodeapi.com/quin/google_sheets/${apiKey}?tabId=Sheet1&perPage=100&page=1&fields=Name,Email`;
+
+    const response = await fetch(url);
     const data = await response.json();
+
+    console.log("Trạng thái:", response.status);
+    console.log("Dữ liệu:", data);
+
+    if (response.status !== 200) {
+      throw new Error(data.info || "Lỗi không xác định");
+    }
+
     res.json(data);
   } catch (error) {
-    console.error("Lỗi khi lấy dữ liệu:", error);
+    console.error("Lỗi khi lấy dữ liệu từ Google Sheets:", error);
     res.status(500).json({ error: "Không lấy được dữ liệu từ Google Sheets" });
   }
 });
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
